88dea6815340c46fc010140725bc9b7889107a53
joseramon.gaitero <joseramon.gaitero@soltel.es>
2023-06-07 22:09:20 +0200

    ranking page with sorting

--- src/app/pages/ranking/ranking.module.ts
+++ src/app/pages/ranking/ranking.module.ts
@@ -8,6 +8,8 @@ import { RankingPageRoutingModule } from './ranking-routing.module';
 
 import { RankingPage } from './ranking.page';
 import { HeaderModule } from "../../components/header/header.module";
+import { PlayerListModule } from 'src/app/components/player-list/player-list.module';
+import { SearchbarModule } from 'src/app/components/searchbar/searchbar.module';
 
 @NgModule({
   declarations: [RankingPage],
@@ -16,7 +18,8 @@ import { HeaderModule } from "../../components/header/header.module";
     FormsModule,
     IonicModule,
     RankingPageRoutingModule,
-    HeaderModule
+    HeaderModule,
+    PlayerListModule,
   ]
 })
 export class RankingPageModule { }
--- src/app/pages/ranking/ranking.page.ts
+++ src/app/pages/ranking/ranking.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/core/models/user.model';
+import { UsersService } from 'src/app/core/services/api/users/users.service';
 
 @Component({
   selector: 'app-ranking',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ranking.page.scss'],
 })
 export class RankingPage implements OnInit {
+  users: User[];
+  sorteredUsers: User[];
+  winsFilter = false;
+  tournamentsFilter = false;
+  sort = null;
 
-  constructor() { }
+  constructor(private usersService: UsersService) { }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+    this.usersService.get().subscribe(res => {
+      this.users = res;
+      this.users[1].wins = 1
+      this.users[1].tournaments.pop()
+      this.sorteredUsers = this.users;
+    })
+  }
+
+  sortedUsers() {
+    let tmp = [...this.users]
+    if (this.sort === null) {
+      this.sorteredUsers = tmp;
+    } else if (!this.sort) {
+      this.sorteredUsers = tmp.sort((a, b) => b.wins - a.wins);
+    }
+    else if (this.sort) {
+      this.sorteredUsers = tmp.sort((a, b) => b.tournaments.length - a.tournaments.length);
+    }
+    console.log(this.sorteredUsers)
+  }
+
+  changeActiveSort(state: boolean) {
+    if (this.sort === null || !this.sort === state) this.sort = state;
+    else if (this.sort === state) this.sort = null;
+    this.sortedUsers()
   }
 
 }
